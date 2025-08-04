65324d42feb1fc43d9aa6f03708be8367a59457f
Johan Santallana <johan921229@gmail.com>
2025-08-03 22:43:02 -0400

    subiendo app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// let pronoun = ['the', 'our'];
+// let adj = ['great', 'big'];
+// let noun = ['jogger', 'racoon'];
+
+// function domainGenerator(){
+//     let result = [];
+    
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let i = 0; i < pronoun.length; i++) {
+//             for (let k = 0; k < noun.length; k++) {
+//                 let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+//                 result.push(domain);
+//             }
+//         }
+//     }
+
+//     return result.join("\n");
+// }
+
+// console.log(domainGenerator());
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+function domainGenerator() {
+    let result = "";
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let x = 0; x < adj.length; x++) {            
+            for(let z = 0; z < noun.length; z++) {
+                result += pronoun[i] + adj[x] + noun[z] + ".com" + "\n";
+            }
+        }
+    }
+    return result;
+}
+
+console.log(domainGenerator());
