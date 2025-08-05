7ad8a6920d51a6118ff6ca12db5fa7dd879a1d6a
Johan Santallana <johan921229@gmail.com>
2025-08-05 16:37:10 -0400

    añadida la siguiente complejidad de un nuevo array con domains

--- app.js
+++ app.js
@@ -17,18 +17,21 @@
 //     return result.join("\n");
 // }
 
-// console.log(domainGenerator());
+//  console.log(domainGenerator());
 
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let ext = [ '.com', '.net', '.us', '.io', '.etc']
 
 function domainGenerator() {
     let result = "";
     for (let i = 0; i < pronoun.length; i++) {
         for (let x = 0; x < adj.length; x++) {            
             for(let z = 0; z < noun.length; z++) {
-                result += pronoun[i] + adj[x] + noun[z] + ".com" + "\n";
+                for (let c = 0; c < ext.length; c++) {
+                    result += pronoun[i] + adj[x] + noun[z] + ext[c] + "\n";
+                }
             }
         }
     }
