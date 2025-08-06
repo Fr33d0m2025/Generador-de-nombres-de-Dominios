b631028b46df8404cef6f07ac7699593d64597eb
Johan Santallana <johan921229@gmail.com>
2025-08-05 22:16:09 -0400

    añadidos los dos retos del proyecto

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
-// let pronoun = ['the', 'our'];
-// let adj = ['great', 'big'];
-// let noun = ['jogger', 'racoon'];
-
-// function domainGenerator(){
-//     let result = [];
-    
-//     for (let j = 0; j < adj.length; j++) {
-//         for (let i = 0; i < pronoun.length; i++) {
-//             for (let k = 0; k < noun.length; k++) {
-//                 let domain = pronoun[i] + adj[j] + noun[k] + ".com";
-//                 result.push(domain);
-//             }
-//         }
-//     }
-
-//     return result.join("\n");
-// }
-
-//  console.log(domainGenerator());
-
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let ext = [ '.com', '.net', '.us', '.io', '.etc']
+let noun = ['choco', 'racoon', 'time', 'print', 'alien', 'fires', 'ghost', 'clown', 'penguin'];
+let ext = ['.com', '.org', '.co', '.io', '.me', '.in', '.es'];
+
+
 
 function domainGenerator() {
-    let result = "";
+
+    let arrayResult = "";
+    let arrayDomainHack = "";
+
     for (let i = 0; i < pronoun.length; i++) {
-        for (let x = 0; x < adj.length; x++) {            
-            for(let z = 0; z < noun.length; z++) {
+        for (let x = 0; x < adj.length; x++) {
+            for (let z = 0; z < noun.length; z++) {
                 for (let c = 0; c < ext.length; c++) {
-                    result += pronoun[i] + adj[x] + noun[z] + ext[c] + "\n";
+                    if (noun[z].endsWith(ext[c].slice(1))) {
+                        arrayDomainHack += pronoun[i] + adj[x] + noun[z].replace(ext[c].slice(1), ext[c])  + "\n";
+                    }
+                    arrayResult += pronoun[i] + adj[x] + noun[z] + ext[c] + "\n";
                 }
             }
         }
     }
-    return result;
+    return arrayResult + arrayDomainHack;
 }
 
 console.log(domainGenerator());
