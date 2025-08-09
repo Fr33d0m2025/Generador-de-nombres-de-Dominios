0a6d6b391c783336fd13db76965efc240174e495
Johan Santallana <johan921229@gmail.com>
2025-08-08 21:34:49 -0400

    se corrigieron los nombres de las variables para el proyecto Lineamientos de Codigo

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['choco', 'racoon', 'time', 'print', 'alien', 'fires', 'ghost', 'clown', 'penguin'];
-let ext = ['.com', '.org', '.co', '.io', '.me', '.in', '.es'];
-
-
+let pronouns = ['the', 'our'];
+let adjetives = ['great', 'big'];
+let nouns = ['choco', 'racoon', 'time', 'print', 'alien', 'fires', 'ghost', 'clown', 'penguin'];
+let extensions = ['.com', '.org', '.co', '.io', '.me', '.in', '.es'];
 
 function domainGenerator() {
 
     let arrayResult = "";
     let arrayDomainHack = "";
 
-    for (let i = 0; i < pronoun.length; i++) {
-        for (let x = 0; x < adj.length; x++) {
-            for (let z = 0; z < noun.length; z++) {
-                for (let c = 0; c < ext.length; c++) {
-                    if (noun[z].endsWith(ext[c].slice(1))) {
-                        arrayDomainHack += pronoun[i] + adj[x] + noun[z].replace(ext[c].slice(1), ext[c])  + "\n";
+    for (let i = 0; i < pronouns.length; i++) {
+        for (let x = 0; x < adjetives.length; x++) {
+            for (let z = 0; z < nouns.length; z++) {
+                for (let c = 0; c < extensions.length; c++) {
+                    if (nouns[z].endsWith(extensions[c].slice(1))) {
+                        arrayDomainHack += pronouns[i] + adjetives[x] + nouns[z].replace(extensions[c].slice(1), extensions[c])  + "\n";
                     }
-                    arrayResult += pronoun[i] + adj[x] + noun[z] + ext[c] + "\n";
+                    arrayResult += pronouns[i] + adjetives[x] + nouns[z] + extensions[c] + "\n";
                 }
             }
         }
